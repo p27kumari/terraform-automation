d890533da7f3731f75faf3ab84fab601afe47b75
Raviraj Singh <raviraj@example.com>
2026-07-27 22:46:47 +0530

    Remove unused bootstrap folder

--- bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Created S3 bucket name"
-  value       = aws_s3_bucket.state_bucket.id
-}
-
-output "dynamodb_table" {
-  description = "Created DynamoDB table name"
-  value       = aws_dynamodb_table.locks.name
-}
