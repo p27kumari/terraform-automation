f24962fe0fc06926f4bc4b786ce82d916caf8954
Raviraj Singh <raviraj@example.com>
2026-07-27 21:47:13 +0530

    updsted tf varssk

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "Pihu-my-terraform-state-bucket-12345"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
