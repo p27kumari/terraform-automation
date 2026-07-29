eadd257e0d290a23cb422badbb466c559f47f3e8
Raviraj Singh <raviraj@example.com>
2026-07-29 12:29:14 +0530

    Added Modulesj

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "pihu-terraform-state-bucket"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    bucket = "pihu-terraform-state-bucket-123"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
   }
 }
